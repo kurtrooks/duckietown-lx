4c316279aac2132e6282719b695de30158f8b104
Kurt <rookskm@gmail.com>
2023-10-31 15:31:08 -0400

    Adding WIP servoing activity

--- visual-lane-servoing/packages/solution/visual_servoing_activity.py
+++ visual-lane-servoing/packages/solution/visual_servoing_activity.py
@@ -3,7 +3,6 @@ from typing import Tuple
 import numpy as np
 import cv2
 
-
 def get_steer_matrix_left_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
     """
     Args:
@@ -14,9 +13,8 @@ def get_steer_matrix_left_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
                             using the masked left lane markings (numpy.ndarray)
     """
 
-    # TODO: implement your own solution here
-    steer_matrix_left = np.random.rand(*shape)
-    # ---
+    steer_matrix_left = np.repeat(np.tile(np.linspace(0,1, shape[1]), (shape[0], 1))[:, :, np.newaxis], 3, axis=2)
+
     return steer_matrix_left
 
 
@@ -30,9 +28,8 @@ def get_steer_matrix_right_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
                              using the masked right lane markings (numpy.ndarray)
     """
 
-    # TODO: implement your own solution here
-    steer_matrix_right = np.random.rand(*shape)
-    # ---
+    steer_matrix_right = np.repeat(np.tile(np.linspace(1,0, shape[1]), (shape[0], 1))[:, :, np.newaxis], 3, axis=2)
+    
     return steer_matrix_right
 
 
@@ -44,10 +41,62 @@ def detect_lane_markings(image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         mask_left_edge:   Masked image for the dashed-yellow line (numpy.ndarray)
         mask_right_edge:  Masked image for the solid-white line (numpy.ndarray)
     """
-    h, w, _ = image.shape
+    height, width, _ = image.shape
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # TODO: implement your own solution here
-    mask_left_edge = np.random.rand(h, w)
-    mask_right_edge = np.random.rand(h, w)
+    # Blur
+    sigma = 3
+    img_gaussian_filter = cv2.GaussianBlur(img,(0,0), sigma)
+
+    # Sobel (edge detect)
+    sobelx = cv2.Sobel(img_gaussian_filter,cv2.CV_64F,1,0)
+    sobely = cv2.Sobel(img_gaussian_filter,cv2.CV_64F,0,1)
+    Gmag = np.sqrt(sobelx*sobelx + sobely*sobely)
+    Gdir = cv2.phase(np.array(sobelx, np.float32), np.array(sobely, dtype=np.float32), angleInDegrees=True)
+ 
+    """ Masks """
+
+    # Gradient
+    mask_sobelx_pos = (sobelx > 0)
+    mask_sobelx_neg = (sobelx < 0)
+    mask_sobely_pos = (sobely > 0)
+    mask_sobely_neg = (sobely < 0)
+
+    # Ground mask 
+    mask_ground = np.ones(img.shape, dtype=np.uint8)
+    mask_ground[:int(width/2)-50,:] = 0
+
+    # Left / right mask
+    mask_left = np.ones(sobelx.shape)
+    mask_left[:,int(np.floor(width/2)):width + 1] = 0
+    mask_right = np.ones(sobelx.shape)
+    mask_right[:,0:int(np.floor(width/2))] = 0
+  
+    # Mag Threshold
+    threshold = 60
+    mask_mag = (Gmag > threshold)
+
+    # Color Mask
+    white_lower_hsv = np.array([0,17,118])  
+    white_upper_hsv = np.array([179,47,255]) 
+    yellow_lower_hsv = np.array([18, 96, 115])
+    yellow_upper_hsv = np.array([40,255,255])
+
+    imghsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask_white = cv2.inRange(imghsv, white_lower_hsv, white_upper_hsv)
+    mask_yellow = cv2.inRange(imghsv, yellow_lower_hsv, yellow_upper_hsv)
+   
+    """    
+    print(mask_ground.shape)
+    print(mask_left.shape)
+    print(mask_mag.shape)
+    print(mask_sobelx_neg.shape)
+    print(mask_sobely_neg.shape)
+    print(mask_yellow.shape)
+    """
+
+    # Output
+    mask_left_edge = mask_ground * mask_left * mask_mag * mask_sobelx_neg * mask_sobely_neg * mask_yellow
+    mask_right_edge = mask_ground * mask_right * mask_mag * mask_sobelx_pos * mask_sobely_neg * mask_white
 
     return mask_left_edge, mask_right_edge

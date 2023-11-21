992ea1c45affbff4efffa3d8896c8108075c9142
Kurt <rookskm@gmail.com>
2023-11-21 09:28:27 -0500

    Submitted object detection hw

--- object-detection/packages/solution/integration_activity.py
+++ object-detection/packages/solution/integration_activity.py
@@ -1,22 +1,20 @@
 from typing import Tuple
 
-
 def DT_TOKEN() -> str:
-    # TODO: change this to your duckietown token
-    dt_token = "PUT_YOUR_TOKEN_HERE"
+    dt_token = "dt1-3nT7FDbT7NLPrXykNJW6pwkBnqd6m6xEgPUtPkStjancVCg-43dzqWFnWd8KBa1yev1g3UKnzVxZkkTbfj33LGDLGg3dfoUaYQpezRtXj5WaBJudv9"
     return dt_token
 
 
 def MODEL_NAME() -> str:
-    # TODO: change this to your model's name that you used to upload it on google colab.
+    # Change this to your model's name that you used to upload it on google colab.
     # if you didn't change it, it should be "yolov5n"
     return "yolov5n"
 
 
 def NUMBER_FRAMES_SKIPPED() -> int:
-    # TODO: change this number to drop more frames
+    # Change this number to drop more frames
     # (must be a positive integer)
-    return 0
+    return 3
 
 
 def filter_by_classes(pred_class: int) -> bool:
@@ -34,10 +32,8 @@ def filter_by_classes(pred_class: int) -> bool:
     Args:
         pred_class: the class of a prediction
     """
-    # Right now, this returns True for every object's class
-    # TODO: Change this to only return True for duckies!
-    # In other words, returning False means that this prediction is ignored.
-    return True
+    #print("Pred class",pred_class)
+    return pred_class == 0 
 
 
 def filter_by_scores(score: float) -> bool:
@@ -45,10 +41,8 @@ def filter_by_scores(score: float) -> bool:
     Args:
         score: the confidence score of a prediction
     """
-    # Right now, this returns True for every object's confidence
-    # TODO: Change this to filter the scores, or not at all
-    # (returning True for all of them might be the right thing to do!)
-    return True
+    #print("Score:",score)
+    return score > 0.2
 
 
 def filter_by_bboxes(bbox: Tuple[int, int, int, int]) -> bool:
@@ -57,5 +51,22 @@ def filter_by_bboxes(bbox: Tuple[int, int, int, int]) -> bool:
         bbox: is the bounding box of a prediction, in xyxy format
                 This means the shape of bbox is (leftmost x pixel, topmost y, rightmost x, bottommost y)
     """
-    # TODO: Like in the other cases, return False if the bbox should not be considered.
-    return True
+    img_width=640
+    img_height=480
+    min_bb_size=1000
+    left_right_buffer=50
+    bottom_threshold = int(0.4*img_height)
+
+    left_x = bbox[0]
+    right_x = bbox[2]
+    top_y = bbox[1]
+    bottom_y = bbox[3]
+
+    box_size = (right_x-left_x)*(bottom_y-top_y)
+    #print(top_y,bottom_y)
+    #print("size:",box_size,box_size > min_bb_size)
+
+    return box_size > min_bb_size \
+           and bottom_y > bottom_threshold \
+           and left_x > left_right_buffer \
+           and right_x < img_width - left_right_buffer
